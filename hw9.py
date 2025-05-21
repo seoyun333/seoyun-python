902b8c96dfcc9dae3c8c33282baff066f11e42fe
seoyun333 <jsy05031448@gmail.com>
2025-05-21 13:14:00 +0900

    Add files via upload

---
+++ hw9.py
@@ -0,0 +1,38 @@
+#개념 확인 과제
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print(f'({self.x}, {self.y})')
+
+        
+class Rectangle:
+    def __init__(self, x1, y1, x2, y2):
+        self.lt = Point(x1, y1)
+        self.rb = Point(x2, y2)
+
+    def show(self):
+        print(f'좌측 상단 꼭짓점: ({self.lt.x}, {self.lt.y})')
+        print(f'우측 하단 꼭짓점: ({self.rb.x}, {self.rb.y})')
+            
+    def getWidth(self):
+        return self.rb.x - self.lt.x
+    
+    def getHeight(self):
+        return self.rb.y - self.lt.y
+
+    def getArea(self):
+        return self.getWidth()*self.getHeight()
+    
+    def getPerimeter(self):
+        return 2*(self.getWidth() + self.getHeight())
+
+
+r1 = Rectangle(5, 5, 20, 10)
+a = r1.getArea()
+p = r1.getPerimeter()
+
+r1.show()
+print(f'\n넓이는 {a}, 둘레는 {p}')
